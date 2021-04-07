8fef69fd42b9d2398d383c85ce056120e459a5a2
Skuty <j.skuthan11@seznam.cz>
2021-04-07 13:00:18 +0200

    Úpravy hráčů - SelfishRandom

--- HitlerGame.py
+++ HitlerGame.py
@@ -9,6 +9,7 @@ import Players
 from Players.DumbOvertFascist import DumbOvertFascist
 from Players.DumbPlayer import DumbPlayer
 from Players.GoodGuy import GoodGuy
+from Players.SelfishRandom import SelfishRandom
 from Players.TrustingPlayer import TrustingPlayer
 
 
@@ -90,12 +91,12 @@ class HitlerGame(object):
         for num in range(self.playernum):
             # name = raw_input("Player #%d's name?\n" % num)
             #if str(roles[0]) == 'liberal':
-            if str(roles[0]) == 'liberal' and pocet == 0:
-                playertype = GoodGuy #choice(self.playertypes)
+            if str(roles[0]) == 'fascist' and pocet == 1:
+                playertype = DumbOvertFascist #choice(self.playertypes)
                 pocet = pocet + 1
             else:
                 # print(roles)
-                playertype = DumbPlayer
+                playertype = SelfishRandom
             #playertype = TrustingPlayer DumbOvertFascist DumbPlayer
             name = playertype.__name__ + ": " + str(num)
             player = playertype(num,
@@ -285,7 +286,7 @@ if __name__ == "__main__":
     games = {"Liberal_policy": 0, "Liberal_kill_Hitler": 0, "Fascist_policy": 0, "Fascist_elect_Hitler": 0}
     print("Beginning play.")
     statCollector = HitlerStats()
-    numgames = 10
+    numgames = 100000
     for ii in tqdm(range(numgames)):
         # print(ii)
         r = newgame(statCollector)
--- Players/SelfishRandom.py
+++ Players/SelfishRandom.py
@@ -1,41 +1,37 @@
 import sys
 from os import path
 from random import getrandbits, choice
-
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from HitlerPlayer import HitlerPlayer, Ja, Nein
 
-name = "DumbOvertFascist"
+name = "SelfishRandom"
 
-
-class DumbOvertFascist(HitlerPlayer):
-    def __init_(self, id, name, role, state):
-        super(DumbOvertFascist, self).__init__(id, name, role, state)
+class SelfishRandom(HitlerPlayer):
+    def __init__(self, id, name, role, state):
+        super(SelfishRandom, self).__init__(id, name, role, state)
 
     def vote(self):
         """
-        Always vote yes to a vote involving at least one fascist if you're fascist, randomly otherwise
+        Just do it randomly :D
+        :return: Random Ja or Nein
         """
-        if self.is_fascist and len(self.fascists) > 0:
-            if self.state.president in self.fascists or self.state.chancellor in self.fascists:
-                return Ja()
-            else:
-                return Nein()
         if bool(getrandbits(1)):
+            #print("Player #%d voting Ja" % self.id)
             return Ja()
         else:
+            #print("Player #%d voting Nein" % self.id)
             return Nein()
 
     def nominate_chancellor(self):
         """
-        Nominate Hitler as chancellor if fascist, randomly otherwise
+        More random!
+        :return: HitlerPlayer
         """
-        if self.is_fascist and not self.is_hitler and self.state.ex_president != self.hitler and self.state.chancellor != self.hitler:
-            return self.hitler
         assert len(self.state.players) > 0
         chancellor = self
         while chancellor == self:
             chancellor = choice(self.state.players)
+        #print("Player #%d choosing chancellor: %s" % (self.id, chancellor.id))
         return chancellor
 
     def view_policies(self, policies):
@@ -44,6 +40,7 @@ class DumbOvertFascist(HitlerPlayer):
         :return:
         """
         pass
+
     @property
     def kill(self):
         """
@@ -51,8 +48,9 @@ class DumbOvertFascist(HitlerPlayer):
         :return:
         """
         kill = self
-        while kill == self or kill.is_dead or (self.is_fascist and len(self.fascists) > 0 and kill in self.fascists):
+        while kill == self or kill.is_dead:
             kill = choice(self.state.players)
+        #print("Player #%d killing: %s" % (self.id, kill.id))
         return kill
 
     def inspect_player(self):
@@ -63,17 +61,18 @@ class DumbOvertFascist(HitlerPlayer):
         inspect = self
         while inspect == self or inspect.is_dead:
             inspect = choice(self.state.players)
+        #print("Player #%d inspecting: %s" % (self.id, inspect.id))
         return inspect
 
     def choose_next(self):
         """
-        Choose the next president - always fascist if fascist!
+        Choose the next president
         :return:
         """
         choose = self
-        while choose == self or choose.is_hitler or choose.is_dead or (
-                self.is_fascist and len(self.fascists) > 0 and choose not in self.fascists):
+        while choose == self or choose.is_dead:
             choose = choice(self.state.players)
+        #print("Player #%d choosing: %s" % (self.id, choose.id))
         return choose
 
     def enact_policy(self, policies):
@@ -105,7 +104,6 @@ class DumbOvertFascist(HitlerPlayer):
         return choices, list(filter(lambda x: x not in choices, policies))[0]
 
     def veto(self, policies):
-        if self.is_fascist:
-            return False
-        else:
-            return True
+        veto = bool(getrandbits(1))
+        #print("Player #%d choosing to veto: %s" % (self.id, veto))
+        return veto
